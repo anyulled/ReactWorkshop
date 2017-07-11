c112d3901ce458e8dccf2c692816faae5d0bd7f9
alrs <anyul.rivas@gft.com>
2017-07-11 13:06:23 +0200

    Integration with redux devtool

--- src/Weather/store.js
+++ src/Weather/store.js
@@ -1,11 +1,12 @@
 /**
  * Created by alrs on 11/07/2017.
  */
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import thunkMiddleware from "redux-thunk";
 import {createLogger} from "redux-logger";
 import weatherReducer from "./weatherReducer";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const loggerMiddleware = createLogger();
 
-export default createStore(weatherReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+export default createStore(weatherReducer, composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware)));

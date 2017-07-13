0e8b2ab06c43aa81b288b643eebf4c0916ad19e7
alrs <anyul.rivas@gft.com>
2017-07-13 17:49:51 +0200

    - adding tests

--- src/WeatherCard.test.js
+++ src/WeatherCard.test.js
@@ -1,11 +1,35 @@
+/**
+ * Created by anyulled on 12/7/17.
+ */
 import React from "react";
-import ReactDOM from "react-dom";
+import {shallow, mount} from "enzyme";
 import WeatherCard from "./WeatherCard";
+import {Panel} from "react-bootstrap";
+import jsdom from 'jsdom';
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.document = doc;
+global.window = doc.defaultView;
 
 describe("WeatherCard", () => {
-    it("renders without crashing", () => {
-        const div = document.createElement("div");
-        ReactDOM.render(<WeatherCard />, div);
+    const props = {
+        weather: {
+            icon: "",
+            date: 1499860800,
+            description: "no description available",
+            minTemp: 0,
+            maxTemp: 99,
+            humidity: 50
+        }
+    };
+    it("should shallow render", () => {
+        const wrapper = shallow(<WeatherCard weather={props}/>);
+        expect(wrapper.find(Panel).length).toEqual(1);
+    });
+
+    it("should render in dom", () => {
+        const wrapper = mount(<WeatherCard weather={props}/>);
+        expect(wrapper.find("img").length).toEqual(1);
+        expect(wrapper.find("p").length).toEqual(4);
     });
 });
 

98c6068116e1fd6be1682ec73aa1a8edf4d0cb52
alrs <anyul.rivas@gft.com>
2017-10-17 19:39:29 +0200

    Putting more logic - select by id - clear data - select by current location

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
+import {configure, mount, render, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-15";
+
+describe("App", () => {
+    const jsdom = require("jsdom").jsdom;
+    const exposedProperties = ["window", "navigator", "document"];
+    global.document = jsdom("");
+    global.window = document.defaultView;
+    Object.keys(document.defaultView).forEach((property) => {
+        if (typeof global[property] === "undefined") {
+            exposedProperties.push(property);
+            global[property] = document.defaultView[property];
+        }
+    });
+    global.navigator = {
+        userAgent: "node.js"
+    };
+
+    configure({adapter: new Adapter()});
+
+    it("renders without crashing", () => {
+        expect(shallow(<App/>).find("div").length).toBe(1);
+        expect(shallow(<App/>).find("PageHeader").length).toBe(1);
+        expect(shallow(<App/>).find("Grid").length).toBe(1);
+
+    });
+
+    it("should mount in a full DOM", () => {
+        expect(mount(<App/>).find(".glyphicon").length).toBe(1);
+    });
+
+    it("should render to static HTML", () => {
+        expect(render(<App/>).text()).toContain("Weather App");
+
+    });
+
+    it("should modify a city", () => {
+        const wrapper = shallow(<App/>);
+        wrapper.instance().modifyCity("Arizona");
+        expect(wrapper.state("city")).toBe("Arizona");
+    });
 });

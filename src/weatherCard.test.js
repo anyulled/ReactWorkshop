19fc5a14e99df3e2a3adc930324cff4456890b98
alrs <anyul.rivas@gft.com>
2017-10-17 11:12:53 +0200

    Weather card tests

--- src/weatherCard.test.js
+++ src/weatherCard.test.js
@@ -19,5 +19,10 @@ describe("WeatherCard", () => {
         };
         const wrapper = shallow(<WeatherCard weather={props}/>);
         expect(wrapper.find("Panel").length).toBe(1);
+        expect(wrapper.find("p").length).toBe(4);
+        expect(wrapper.find("p").first().text()).toContain("Description");
+        expect(wrapper.find("p").at(1).text()).toContain("Minimum");
+        expect(wrapper.find("p").at(2).text()).toContain("Maximum");
+        expect(wrapper.find("p").last().text()).toContain("Humidity");
     });
 });

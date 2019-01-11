c2142114e3a1dc9a9ba8efa87d032a38714d74f9
alrs <anyul.rivas@gft.com>
2019-01-11 15:21:28 +0100

    fixing travis build

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,8 @@
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
 const mockGeolocation = {
     getCurrentPosition: jest.fn(() => ({
         coords: {

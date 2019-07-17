11be049ddb0db7cafec6d9dec4b46e13973da96d
anyulled <anyulled@gmail.com>
2019-07-17 21:27:49 +0200

    upgrading to react scripts v3

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
-import "bootstrap/dist/fonts/glyphicons-halflings-regular.ttf";
-import "bootstrap/dist/fonts/glyphicons-halflings-regular.eot";
-import "bootstrap/dist/fonts/glyphicons-halflings-regular.svg";
-import "bootstrap/dist/fonts/glyphicons-halflings-regular.woff";
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();

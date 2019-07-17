00cb88d5ae88f212f906650ff0f6b651d0c0cb78
anyulled <anyulled@gmail.com>
2019-07-17 21:59:15 +0200

    updating enzyme adapter

--- src/weatherCard.test.js
+++ src/weatherCard.test.js
@@ -1,7 +1,7 @@
 import WeatherCard from "./WeatherCard";
 import React from "react";
 import {configure, shallow} from "enzyme";
-import Adapter from "enzyme-adapter-react-15";
+import Adapter from "enzyme-adapter-react-16";
 
 describe("WeatherCard", () => {
     beforeAll(() => {

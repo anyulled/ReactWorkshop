b52b85d2960e445bd809e27c45943792a0737f62
anyulled <anyulled@gmail.com>
2019-07-22 00:35:24 +0200

    fixing tests files, further refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "./App";
-import {configure, mount, render, shallow} from "enzyme";
+import {configure, mount, render} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 describe("App", () => {
@@ -9,9 +9,9 @@ describe("App", () => {
     });
 
     it("renders without crashing", () => {
-        expect(shallow(<App/>).find("div").length).toBe(1);
-        expect(shallow(<App/>).find("PageHeader").length).toBe(1);
-        expect(shallow(<App/>).find("Grid").length).toBe(1);
+        expect(mount(<App/>).find("div").length).toBe(1);
+        expect(mount(<App/>).find("PageHeader").length).toBe(1);
+        expect(mount(<App/>).find("Grid").length).toBe(1);
     });
 
     it("should mount in a full DOM", () => {
@@ -23,7 +23,7 @@ describe("App", () => {
     });
 
     it("should modify a city", () => {
-        const wrapper = shallow(<App/>);
+        const wrapper = mount(<App/>);
         wrapper.instance().modifyCity("Arizona");
         expect(wrapper.state("city")).toBe("Arizona");
     });
--- src/LocationButtonGroup.js
+++ src/LocationButtonGroup.js
@@ -2,13 +2,10 @@ import {Button, ButtonGroup} from "react-bootstrap";
 import PropTypes from "prop-types";
 import React from "react";
 
-export function LocationButtonGroup(props) {
-    return <ButtonGroup>
-        <Button bsStyle="primary" type="button" onClick={props.clearDataHandler}>Clear data</Button>
-        <Button bsStyle="info" type="button" onClick={props.currentLocationHandler}>Current Location
-            data</Button>
-    </ButtonGroup>;
-}
+export const LocationButtonGroup = props => <ButtonGroup>
+    <Button bsStyle="primary" type="button" onClick={props.clearDataHandler}>Clear data</Button>
+    <Button bsStyle="info" type="button" onClick={props.currentLocationHandler}>Current Location data</Button>
+</ButtonGroup>;
 
 LocationButtonGroup.propTypes = {
     clearDataHandler: PropTypes.func.isRequired,

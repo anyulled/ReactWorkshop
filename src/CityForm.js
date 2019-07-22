a344cb4fb5cdffe29b1f46321571a5823764e8bd
alrs <anyul.rivas@gft.com>
2019-07-22 15:44:00 +0200

    implementing react hooks
    improving city form coverage.

--- src/CityForm.js
+++ src/CityForm.js
@@ -2,22 +2,21 @@ import {Col, ControlLabel, Form, FormControl, FormGroup} from "react-bootstrap";
 import PropTypes from "prop-types";
 import React from "react";
 
-export function CityForm(props) {
-    return <Form horizontal>
-        <FormGroup controlId="formControlsSelect">
-            <Col xs={3}><ControlLabel>Select City</ControlLabel></Col>
-            <Col xs={9}>
-                <FormControl id="citySelect" componentClass="select" placeholder="select" value={props.value} onChange={props.onChange}>
-                    <option value="">Select</option>
-                    <option value="360630">El Cairo</option>
-                    <option value="2643743">London</option>
-                    <option value="3128759">Barcelona</option>
-                    <option value="3646738">Caracas</option>
-                </FormControl>
-            </Col>
-        </FormGroup>
-    </Form>;
-}
+const CityForm = props => <Form horizontal>
+    <FormGroup controlId="formControlsSelect">
+        <Col xs={3}><ControlLabel>Select City</ControlLabel></Col>
+        <Col xs={9}>
+            <FormControl id="citySelect" componentClass="select" placeholder="select" value={props.value} onChange={props.onChange}>
+                <option value="">Select</option>
+                <option value="360630">El Cairo</option>
+                <option value="2643743">London</option>
+                <option value="3128759">Barcelona</option>
+                <option value="3646738">Caracas</option>
+            </FormControl>
+        </Col>
+    </FormGroup>
+</Form>;
+export default CityForm;
 
 CityForm.propTypes = {
     value: PropTypes.any.isRequired,
---
+++ src/CityForm.test.js
@@ -0,0 +1,22 @@
+import CityForm from "./CityForm";
+import {Form} from "react-bootstrap";
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+describe("<CityForm/>", () => {
+    beforeAll(() => {
+        configure({adapter: new Adapter()});
+    });
+    it("should mount correctly", () => {
+        const props = 360630;
+        const onChangeHandler = jest.fn();
+        const wrapper = shallow(<CityForm value={props} onChange={onChangeHandler}/>);
+        expect(wrapper.find("Form").length).toBe(1);
+        expect(wrapper.find("FormGroup").length).toBe(1);
+        expect(wrapper.find("FormControl").length).toBe(1);
+        expect(wrapper.find("FormControl").prop("value")).toBe(props);
+        expect(wrapper.find("FormControl").prop("onChange")).toBe(onChangeHandler);
+        expect(wrapper.find("option").length).toBe(5);
+    });
+});

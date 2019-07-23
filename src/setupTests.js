e12e5de23a67a56bf812787d18b946e8944849db
alrs <anyul.rivas@gft.com>
2019-07-23 17:33:19 +0200

    proper geolocation mocking.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,15 +1,16 @@
-import { configure } from "enzyme";
+import {configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
-
 const mockGeolocation = {
-    getCurrentPosition: jest.fn(() => ({
-        coords: {
-            latitude: 41.486201,
-            longitude: 2.0378555
-        }
-    }))
+    getCurrentPosition: jest.fn()
+        .mockImplementation((success) => Promise.resolve(success({
+            coords: {
+                latitude: 41.486201,
+                longitude: 2.0378555
+            }
+        })))
 };
+
 global.navigator = {};
 global.navigator.geolocation = mockGeolocation;
